78baeb52295a9fc294a4fb9d82a436210d1b4a96
JS <js.developer.undefined@gmail.com>
2012-12-04 12:47:32 -0700

    #4:  The $basedir variable is wrong during a prod build.

--- bin/deployDev.sh
+++ bin/deployDev.sh
@@ -1,5 +1,5 @@
 basedir="/XforJ.com"
 
-ant -f $(dirname $PWD)/build.xml -Dparam.basedir=/XforJ.com
+ant -f $(dirname $PWD)/build.xml -Dparam.basedir=/XforJ.com/
 
 google-chrome --new-window file:///XforJ.com/index.html
